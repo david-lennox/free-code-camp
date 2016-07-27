ae538c6dcd4de62ef1c42cb81ec6a87f3b08ae88
David Walker <david.lennox@live.com>
2016-07-27 14:39:54 +1000

    Recipe App update - working with test data and no ability to add content. ================ Cut down and change architecture - see LEARNING NOTES in file.

--- public/scripts/fcc_recipe_app_local_storage-xps.js
+++ public/scripts/fcc_recipe_app_local_storage-xps.js
@@ -1,126 +1,44 @@
 /**
- * PATTERN EXPLANATION
- * We have a parent that controls the list. The parent creates a new blank item and adds it to
- * the list. Then the child has a `props` property with the original 'new' name. The child is 
- * responsible for keeping track of its new name (changed by the user) and then when the user 
- * clicks 'update' the child sends the parent its new details and its original `props` name so 
- * the parent can replace the old with the new. 
+ * LEARNING POINT
+ * Keep most components as 'pure' meaning they have no state and just render the props passed down.
+ * Previous version of this file tried to spread the State model across the RecipeBox, Recipe, and 
+ * Ingredient classes. This is wrong. It should all be contained in the first because it is not 
+ * possible to make a change to Ingredient that is not to be reflected in the RecipeBox. 
+ * 
+ * This app may be developed in a very similar way to the Kanban-App of the Apress Pro React book.
+ * In particular have a look how that app creates the list of tasks in CheckList.js (chapter 4 version).
+ * Note that a Task component is passed (as props) functions defined in the RecipeBox which expect a
+ * taskId and a CardId. These equate to an ingredientId, and a RecipeId.
+ * 
+ * Note the method of 'binding' the context and the arguments to functions. So this is a way of passing
+ * in the arguments without actually calling the function. The bind function returns a new function that 
+ * will be called with the specified arguments.
+ * 
+ * DATA STRUCTURE:
+ * The code is coupled to the data structure, which is described by example in getInitialRecipes().
  */
 
-var Ingredient = React.createClass({
-    getInitialState: function(){
-        return {
-            name: this.props.ingredient.name || '',
-            qtyAndUnit: this.props.ingredient.qtyAndUnit || '',
-         }
-    },
-    render: function(){
-        //debugger;
-
-        return this.props.current === true ? 
-            (
-                <div className="ingredient">
-                    Name: <input type="text" value={this.state.name} onChange={this.nameFieldChaange} />
-                    Qty: <input type="text" value={this.state.qtyAndUnit} onChange={this.qtyAndUnitFieldChange}/>
-                    <button onClick={this.update}>Update</button>
-                </div>
-            )    : 
-            (
-                <p id="this.state.name" onClick={this.makeCurrent}>{this.props.name}: {this.props.qtyAndUnit}</p>
-            )
-    },
-    makeCurrent: function(){
-        this.props.setCurrentIngredient(this.props.name);
-    },
-    nameFieldChaange: function(e){
-        this.setState({name: e.target.value});
-    },
-    qtyAndUnitFieldChange: function(e){
-        this.setState({qtyAndUnit: e.target.value});
-    },
-    update: function(){
-        this.props.updateIngredient(this.props.name, {name: this.state.nameField, qtyAndUnit: this.state.qtyAndUnitField});
-    }
-});
-
 var Recipe = React.createClass({
-
-    getInitialState: function(){
-        return {title: this.props.recipe.title, ingredients: this.props.recipe.ingredients, currentIngredient: ''}
-    },
-
     render: function(){
 
-        self = this;
-
-        var ingredientList = Object.keys(self.state.ingredients).map(function(ingredientId){
-            //debugger;
-           return <Ingredient
-                key={ingredientId}
-                ingredient={self.state.ingredients[ingredientId]}
-                current={self.state.currentIngredient === ingredientId}
-                updateIngredient={self.updateIngredient}
-                removeIngredient={self.removeIngredient}
-                setCurrentIngredient={self.setCurrentIngredient} />
+        var ingredients = this.props.recipe.ingredients.map(function(ingredient, index){
+           return <li key={ingredient}>{ingredient}</li>
         });
-        return self.props.current === true ?
-            //debugger;
-             (
-                <div id={self.state.title} className='recipe'>
-                    <input type='text' value={self.state.title} onChange={this.handleTitleChange} />
-                    {ingredientList}
-                    <button onClick={self.update}>Update</button>
-                </div> ) :
-            
-         (
-            <div id={self.state.title} className='recipe'>
-                <h3>{self.state.title}</h3>
-                <button onClick={self.addIngredient}>Add Ingredient</button>
-                {ingredientList}
-                <button onClick={self.makeCurrent}>Edit</button>
-                <button onClick={self.remove}>Delete</button>
+
+        return (
+            <div id={this.props.recipe.name} className='recipe'>
+                <h3>{this.props.recipe.name}</h3>
+                <ul className='ingredients'>{ingredients}</ul>
+                <button onClick={this.props.deleteRecipe.bind(null, this.props.name)}>Delete</button>
             </div>
         )
-    },
-
-    remove: function(){
-        this.props.removeRecipe(this.state.title);
-    },
-
-    handleTitleChange: function(e){
-        this.setState({title: e.target.value});
-    },
-
-    makeCurrent: function(){
-        this.props.setCurrentRecipe(this.props.recipe.title); // or this.state.title? Is it possible they're different?
-    },
-
-    update: function(){
-        //debugger;
-        this.props.updateRecipe(this.props.recipeId, {title: this.state.title, ingredients: this.state.ingredients});
-    },
-
-    addIngredient: function(){
-        var newList = this.state.ingredients;
-        newList['new'] = {name: '', qtyAndUnit: ''};
-        this.setState({ingredients: newList, currentIngredient: 'new'});
-    },
-
-    updateIngredient: function(oldName, ingr){  
-        var newList = this.state.ingredients;
-        delete newList[oldName];
-        newList[ingr.name] = ingr;
-        this.setState({ingredients: newList, currentIngredient: ''});
-    },
-    setCurrentIngredient: function(name){
-        this.setState({currentIngredient: name})
-    },
+    }
 });
 
 var RecipeBox = React.createClass({
     getRecipes: function(){
         // TODO - confirm received data is in the correct format.
-        this.setState({recipes: JSON.parse(localStorage.getItem("_dave004_recipes"))});
+        //this.setState({recipes: JSON.parse(localStorage.getItem("_dave004_recipes"))});
     },
 
     getInitialState: function(){
@@ -128,79 +46,61 @@ var RecipeBox = React.createClass({
     },
 
     componentWillMount: function(){
-        this.getRecipes();
-    },
-
-    setCurrentRecipe: function(title){
-        this.setState({currentRecipe: title});
-    },
-
-    removeRecipe: function(recipeId){
-        var newRecipeList = this.state.recipes;
-        delete newRecipeList[recipeId];
-        this.setState({recipes: newRecipeList});
-        localStorage.setItem('_dave004_recipes', newRecipeList);
-    },
-
-    addRecipe: function(){
-        var newList = this.state.recipes || {};
-        newList['new'] = {title: '', ingredients: {}};
-        this.setState({recipes: newList, currentRecipe: 'new'});
-    },
-
-    updateRecipe: function(oldTitle, recipe){
-        var newList = this.state.recipes;
-        delete newList[oldTitle];
-        //debugger;
-        newList[recipe.title] = recipe;        
-        this.setState({recipes: newList});
-        localStorage.setItem('_dave004_recipes', JSON.stringify(newList));
+        var firstRecipes = this.getFirstRecipes();
+        this.setState({recipes: firstRecipes})
     },
 
     render: function(){
         var self = this;
-
-        if(self.state.recipes != null){
-            //debugger;
-            var recipeElements = Object.keys(self.state.recipes).map(function(recipeId){
+        if(this.state.recipes != null){
+            var recipes = this.state.recipes.map(function(recipe){
                 return (
                     <Recipe
-                    current={recipeId === self.state.currentRecipe}
-                    recipeId={recipeId}
-                    recipe={self.state.recipes[recipeId]}
-                    key={recipeId}
-                    removeRecipe={self.removeRecipe}
-                    setCurrentRecipe={self.setCurrentRecipe}
-                    updateRecipe={self.updateRecipe}/>
+                    key={recipe.name}
+                    recipe={recipe}
+                    deleteRecipe={self.deleteRecipe}/>
                 )
             });
         }
-        else var recipeEls = "No recipes in local storage";
+        else var recipes = "No recipes in local storage";
 
         return (
             <div className="recipeBox">
             <h1>The Recipe Box</h1>
             <button onClick={this.addRecipe}>Add New</button>
             <h2>Recipes</h2>
-            {recipeElements}
+            {recipes}
             </div>
         )
-    }
+    },
+    deleteRecipe: function(recipeIndex){
+        var newList = this.state.recipes;
+        newList.splice(recipeIndex, 1);
+        this.setState({recipes: newList});
+    },
+    getFirstRecipes: function() {
+    return [{
+            name: "Spaghetti Bolognaisse",
+            ingredients: [
+                "2 chopped onions",
+                "3 cloves of garlic",
+                "4 tomatoes"
+            ],
+            description: 'Put it all in a pot and cook for about 20 minutes'
+        },{
+            name: "Curry",
+            ingredients: [
+                "2 tbs curry powder",
+                "500g chicken",
+                "1L chicken stock"
+            ],
+            description: 'Put it all in a pot and cook for about 20 minutes'
+        }
+    ] 
+}
 });
 
 ReactDOM.render(
     <RecipeBox />,
     document.getElementById('content')
 );
-
-
-var recipeStructure = {
-    "Spaghetti Bolognaisse": {
-        ingredients: {
-            onion: 1,
-            carrot: 2,
-            garlic: '3 cloves'
-        },
-        description: 'Put it all in a pot and cook for about 20 minutes'
-    }
-}
