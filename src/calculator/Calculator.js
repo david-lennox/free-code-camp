a00f8119a58a65a44eeeb0e37b90543f75ffd2da
David Walker <david.lennox@live.com>
2016-10-14 14:50:57 +1100

    # Calculator App to 90%

--- src/calculator/Calculator.js
+++ src/calculator/Calculator.js
@@ -1,54 +1,78 @@
 import React from 'react';
+import './calculator.css';
 
 var Calculator = React.createClass({
+
+    getInitialState: function(){
+        return {equationStr: '', currentInputStr: '', answer: 0}
+    },
+
     render: function(){
-        var numbers = [];
-        for(var i = 9; i >=0; i--){
-            numbers.push(
-                <Number number={i} key={i} numberButtonEvent={this.numberButtonEvent}/>
-            )
-        }
         return (
             <div className="calculator">
-                <Screen 
-                    mainDisplay={this.state.inputAndAnswer}
-                    subDisplay={this.state.equationTxt} 
-                />
-                {numbers}
+                <div>Equation: {this.state.equationStr}</div>
+                <div>Answer: {this.state.answer}</div>
+                <table>
+                    <tbody>
+                        <CalcRow updateEquation={this.updateEquation} buttonValues={["operation-clear", "/", "*", "operation-delete"]} />
+                        <CalcRow updateEquation={this.updateEquation} buttonValues={[7, 8, 9, " - "]} />
+                        <CalcRow updateEquation={this.updateEquation} buttonValues={[4, 5, 6, " + "]} />
+                        <CalcRow updateEquation={this.updateEquation} buttonValues={[1, 2, 3, "operation-brackets"]} />
+                        <CalcRow updateEquation={this.updateEquation} buttonValues={[0, ".", " * -1 ", "operation-equals"]} />
+                    </tbody>
+                </table>
             </div>
         )
     },
-    getInitialState: function(){
-        return {inputAndAnswer: 55, equationTxt: ""};
-    },
-    numberButtonEvent: function(value){
-        this.setState({equationTxt: this.state.equationTxt + " " + value})
+    updateEquation: function(value){
+        if(isNaN(value) && value.match(/^operation/)){
+            switch(value.split('-')[1]){
+                case 'clear':
+                    this.setState({equationStr: '', answer: ''});
+                    break;
+                case 'delete':
+                    this.setState({equationStr: this.state.equationStr.substring(0, this.state.equationStr.length - 1)});
+                    break;
+                case 'equals':
+                    // eslint-disable-next-line
+                    this.setState({answer: eval(this.state.equationStr)});
+                    break;
+                default:
+                    console.log('something went wrong!');
+            }
+        }
+        else this.setState({equationStr: this.state.equationStr + value})
+
     }
 });
 
-var Number = React.createClass({
+var CalcRow = React.createClass(({
     render: function(){
-        return <button onClick={this.handleClick} className="calc-num">{this.props.number}</button>
-    },
-    handleClick: function(){
-        this.props.numberButtonEvent(this.props.number);
-    }
-});
+        var tableDataCells = this.props.buttonValues.map(v => {
+            var symbol;
+            if(isNaN(v) && v.match(/^operation/)) {
+                switch(v.split('-')[1]){
+                    case 'delete':
+                        symbol = 'DEL';
+                        break;
+                    case 'clear':
+                        symbol = 'CLR';
+                        break;
+                    case 'equals':
+                        symbol = '=';
+                        break;
+                    default:
+                        console.log("what are you missing");
+                        break;
+                }
+            }
+            else symbol = v;
 
-var Screen = React.createClass({
-    render: function(){
-        return (
-            <div className="calc-screen">
-                <div className="main-display">
-                    {this.props.mainDisplay}
-                </div>
-                <div className="sub-display">
-                    {this.props.subDisplay}
-                </div>
-            </div>
-        )
+            return <td key={v} className="calc-button" onClick={this.props.updateEquation.bind(null, v)}>{symbol}</td>
+        });
+        return <tr>{tableDataCells}</tr>
     }
-});
+}));
 
 export default Calculator;
 
